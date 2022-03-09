da504f57837ea1941cf188f9763d96b93a920b95
lvicos <lvivancos2nd-dev@yahoo.fr>
2022-03-09 13:34:22 +0700

    project structure and DB connection

---
+++ app.js
@@ -0,0 +1,36 @@
+require('./db/connect')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get("/hello", (req, res) => {
+    res.send('Task manager app')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')         - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+const PORT = 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
---
+++ db/connect.js
@@ -0,0 +1,15 @@
+const mangoose = require('mongoose')
+
+const connectDB = (url) => {
+    return mangoose.connect(url, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+    })
+}
+
+module.exports = connectDB
+
+
+
