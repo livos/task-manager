1538e9302656665f1efa4d43a82515235211cdaf
lvicos <lvivancos2nd-dev@yahoo.fr>
2022-03-11 16:42:59 +0700

    custom 404

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 // middleware
 app.use(express.static('./public'))
@@ -12,11 +13,7 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tasks', tasks)
 
-// app.get('/api/v1/tasks')         - get all the tasks
-// app.post('/api/v1/tasks')        - create a new task
-// app.get('/api/v1/tasks/:id')     - get single task
-// app.patch('/api/v1/tasks/:id')   - update task
-// app.delete('/api/v1/tasks/:id')  - delete task
+app.use(notFound)
 
 const PORT = 3000
 
